b552bf42b26149e5f16ee9adbda68d74bfb8e7b7
Bourhan Dernayka <35309929+Bourhano@users.noreply.github.com>
2022-12-25 16:08:14 +0100

    Fix numpy array dtype
    I propose a fix to this line to consider defining the dtype of the copied object as 'a16' to treat it as an object that contains strings instead of the np.object dtype that is now deprecated.
    
    In fact, without this fix, the code will crash and the plot_policy function will not show any plots :)

--- utils.py
+++ utils.py
@@ -20,7 +20,7 @@ def plot_policy(probs_or_qvals, frame, action_meanings=None):
         action_meanings = {0: 'U', 1: 'R', 2: 'D', 3: 'L'}
     fig, axes = plt.subplots(1, 2, figsize=(12, 6))
     max_prob_actions = probs_or_qvals.argmax(axis=-1)
-    probs_copy = max_prob_actions.copy().astype(np.object)
+    probs_copy = max_prob_actions.copy().astype('a16')
     for key in action_meanings:
         probs_copy[probs_copy == key] = action_meanings[key]
     sns.heatmap(max_prob_actions, annot=probs_copy, fmt='', cbar=False, cmap='coolwarm',
